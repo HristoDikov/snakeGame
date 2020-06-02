d0c7dc73463b2791df6800e02cd7aa5038be06a5
HristoDikov? <dikoww7@gmail.com>
2020-06-02 19:16:56 +0300

    Implement Snake functionality

--- js/modules/SnakeBody.js
+++ js/modules/SnakeBody.js
@@ -3,8 +3,9 @@ import GameObject from './GameObject.js';
 //Snake body definition
 //This class represents snake's body parts.
 export default class SnakeBody extends GameObject {
-    constructor() {
-        // lightgreen
+    constructor(x, y) {
+        super(x, y, false);
+        this.color = 'lightgreen';
     }
 
     //SnakeBody don't need update, but each object must have update method
