8ee4229a9f42bcd3035698de00e6e26c05de7a65
HristoDikov? <dikoww7@gmail.com>
2020-06-02 18:04:03 +0300

    Implement Stone

--- js/modules/Stone.js
+++ js/modules/Stone.js
@@ -3,8 +3,9 @@ import GameObject from './GameObject.js';
 //Stones definition
 //This class represents stones which the snake must avoid
 export default class Stone extends GameObject {
-    constructor() {
-        // grey
+    constructor(x, y) {
+        super(x, y, false, document.getElementById('stone'));
+        this.color = 'grey';
     }
 
     //Stones don't need update, but each object must have update method
