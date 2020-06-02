c7444b97260853af105cbd093e8ba2cc4e338ac6
HristoDikov? <dikoww7@gmail.com>
2020-06-02 18:09:51 +0300

    Implement Food

--- js/modules/Food.js
+++ js/modules/Food.js
@@ -4,13 +4,19 @@ import * as Helper from './helper.js';
 //Food definition
 //This class represents Food objects in the game. It inherits GameObject
 export default class Food extends GameObject {
-    constructor() {
-        // red
+    constructor(x, y) {
+        super(x, y, true, document.getElementById('apple'))
+        this.color = 'red';
     }
 
     //Specific update method for all instances of Food class. If destroyed, 
     // food disappears and appears on different spot
     update() {
+        if (this.isDestroyed) {
+            this.position.X = Helper.getRandomPositionX(0, Helper.FieldSize.WIDTH - this.size.WIDTH);
+            this.position.Y = Helper.getRandomPositionY(0, Helper.FieldSize.HEIGHT - this.size.HEIGHT);
 
+            this.isDestroyed = false;
+        }
     }
 }
