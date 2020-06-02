7486bc6c029a6d4e6c5e49b3addf8ab768ba7e3c
HristoDikov? <dikoww7@gmail.com>
2020-06-02 17:45:48 +0300

    Implement GameField functionality

--- js/modules/GameField.js
+++ js/modules/GameField.js
@@ -1,15 +1,20 @@
-import * as Helper from './helper.js'; 
+import * as Helper from './helper.js';
 
 //Game Field definition
 // This class represents canvas element on which the game objects are drawn
 export default class GameField {
     // constructor receives canvas, has size and context
-    constructor() {
+    constructor(canvas) {
+        canvas.height = Helper.FieldSize.HEIGHT;
+        canvas.width = Helper.FieldSize.WIDTH;
 
-    }    
+        this.context = canvas.getContext('2d');
+    }
 
     // receives game objects, clears canvas and draws them
-    draw() {
-        
+    draw(gameObject) {
+        for(var i = 0; i < gameObject.length; i++){
+            gameObject[i].draw(this.context);
+        }
     }
 }
